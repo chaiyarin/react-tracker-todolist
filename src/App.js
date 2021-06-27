ea5f25fd5fa7a76a02ae4e51b7b65e27b94c9400
Chaiyarin <chaiyarin.aof@gmail.com>
2021-06-27 21:07:56 +0700

    แสดงผล Task ได้แล้ว และก็สามารถ ลบ task ได้ด้วยการ useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
+import logo from './logo.svg'
+import './App.css'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Document Appointment',
+      day: 'Feb',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Document Appointment 2',
+      day: 'Feb',
+      reminder: true
+    }
+    , {
+      id: 3,
+      text: 'Document Appointment 3',
+      day: 'Feb',
+      reminder: true
+    }
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
   const name = 'Chaiyarin';
   const x = true;
   return (
     <div className="container">
       <Header title="Chaiyarin"></Header>
+
+      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'ไม่มีข้อมูล'}
+      
     </div>
   );
 }
