800ca58903f7fc00cab9c8dc24807f8cb3b1d2d2
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2021-06-30 22:59:05 +0700

    insert update delete by rest api useEffect

--- src/App.js
+++ src/App.js
@@ -3,34 +3,30 @@ import './App.css'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 
   const [showAddTask, setShowAddTask] = useState(false)
 
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Document Appointment',
-      day: 'Feb',
-      reminder: true
-    },
-    {
-      id: 2,
-      text: 'Document Appointment 2',
-      day: 'Feb',
-      reminder: true
-    }
-    , {
-      id: 3,
-      text: 'Document Appointment 3',
-      day: 'Feb',
-      reminder: true
-    }
-  ])
+  const [tasks, setTasks] = useState([])
 
-  const deleteTask = (id) => {
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
     setTasks(tasks.filter(task => task.id !== id))
   }
 
@@ -43,10 +39,16 @@ function App() {
   }
 
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(task) // javascript object to json string 
+    })
+    const data = await res.json()
+    setTasks([...tasks, data])
   }
 
   const name = 'Chaiyarin';
